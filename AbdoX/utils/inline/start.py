5da379eadf9acbc72fd25ae706b7081d84f7dc99
DEV-E3DAM <139201770+Darksourcebot@users.noreply.github.com>
2024-05-15 20:41:41 +0300

    Add files via upload

---
+++ AbdoX/utils/inline/start.py
@@ -0,0 +1,36 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from AbdoX import app
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url= "https://t.me/PO_UV"),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        
+        [
+            InlineKeyboardButton(text="مطور السورس ", url=f"https://t.me/DAD_E3DAM"),
+            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url=f"https://t.me/PO_UV"), 
+        ],
+        [
+            
+            InlineKeyboardButton(text="𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url=f"https://t.me/CG_G11") , 
+        ],
+    ]
+    return buttons
